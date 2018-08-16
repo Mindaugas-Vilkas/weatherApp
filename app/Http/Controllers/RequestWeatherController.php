c3786cf7e56f537fe74a937a7ff817b12d02ffdf
Mindaugas Vilkas <mindaugasVilkaas@gmail.com>
2018-08-16 16:12:22 +0300

    base controller for development

--- app/Http/Controllers/RequestWeatherController.php
+++ app/Http/Controllers/RequestWeatherController.php
@@ -19,15 +19,10 @@ class RequestWeatherController extends Controller
     public function getWeather(Request $request)
     {
         $appid = '88e2bf476f822117752bf6e87fe1e69c';
-        $city = 'kaunas'; // TODO: get this dynamically from the request itself.
+        $city = $request->requestCityWeather;
         $client = new GuzzleHttp\Client();
         $res = $client->request('GET', "http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$appid}");
         $resBody = json_decode((string)$res->getBody());
-//        echo'<pre>';
-//        echo'StatusCode';
-//        print_r($res->getStatusCode());
-//        print_r($resBody);
-//        echo'<br />WindDirect<br />';
 
         if(isset($resBody->wind->deg)){
             $windDirection = (int)$resBody->wind->deg / 45;
@@ -38,10 +33,13 @@ class RequestWeatherController extends Controller
         $directions = ['Northwards', 'Northeastwards', 'Eastwards', 'Southeastwards', 'Southwards', 'Southwestwards', 'Westwards', 'Northwestwards'];
         $windDirection = $directions[$windDirection];
 
-//        print_r($windDirection);
-//        exit;
-
-
         return view('display', ['requestedCity' => $city, 'weatherArray'=>$resBody, 'windDirection'=>$windDirection]);
+    }
+
+    public function createAlert() {
+
+
+
+        return view('signedup');
     }
 }
