b9b53bbd74da39bc62f6fe89043b5f7f9d8e4bb6
Mindaugas Vilkas <mindaugasVilkaas@gmail.com>
2018-08-16 15:09:43 +0300

    transforming wind direction to avoid math on the view

--- app/Http/Controllers/RequestWeatherController.php
+++ app/Http/Controllers/RequestWeatherController.php
@@ -1,15 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Vartotojas
- * Date: 2018-08-16
- * Time: 14:00
- */
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use GuzzleHttp;
 
-class RequestWeatherController
+class RequestWeatherController extends Controller
 {
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  Request
+     * @return View
+     */
+    public function getWeather(Request $request)
+    {
+        $appid = '88e2bf476f822117752bf6e87fe1e69c';
+        $city = 'kaunas'; // TODO: get this dynamically from the request itself.
+        $client = new GuzzleHttp\Client();
+        $res = $client->request('GET', "http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$appid}");
+        $resBody = json_decode((string)$res->getBody());
+        echo'<pre>';
+        echo'StatusCode';
+        print_r($res->getStatusCode());
+        echo'<br />WindDirect';
+        if(isset($resBody->wind->deg)){
+            $windDirection = $resBody->wind->deg / 45;
+        } else {
+            $windDirection = 404;
+        }
 
+        print_r($windDirection);
+
+
+
+        exit;
+        return view('display', ['requestedCity' => $city, 'weatherArray'=>$resBody, 'windDirection'=>$windDirection]);
+    }
 }
