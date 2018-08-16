e257975d6a212f26dff581a0f7c6ec74f21eec29
Mindaugas Vilkas <mindaugasVilkaas@gmail.com>
2018-08-16 16:11:23 +0300

    rationalizing and prepping for further development

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,8 +1,27 @@
 @extends('base')
 
 @section('content')
-    <form action="/get-weather" method="post">
-        <label for="requestWeather">Enter city name</label>
-        <input name="requestWeather" id="requestWeather" type="text">
+    <form action="/get-weather" method="get">
+        <label for="requestCityWeather">Enter city name</label>
+        <input name="requestCityWeather" id="requestCityWeather" type="text">
+    </form>
+    <br />
+    <br /> <br />
+    <br />
+    <h3>Sign up to be alerted about dangerous wind speeds</h3>
+    <form action="/alert" method="post">
+        <label for="requestAlertCity">Enter city name</label>
+        <input name="requestAlertCity" id="requestAlertCity" type="text">
+        <br />
+        <br />
+        <label for="requestAlertEmail">Enter your email</label>
+        <input name="requestAlertEmail" id="requestAlertEmail" type="text">
+    </form>
+    <br />
+    <br /> <br />
+    <br />
+    <form>
+        <label for="fakeIt">Fake it ?</label>
+        <input name="fakeIt" id="fakeIt" value="fakeIt" type="submit">
     </form>
 @stop
